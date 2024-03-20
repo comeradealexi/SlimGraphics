53671cf5f2fb1f080c45e5f2b0ab5de310b71839
AlexHoughton <houghton1411@aol.com>
2024-03-20 17:58:07 +0000

    upload heaps WIP

--- SlimGraphicsApplication/UploadHeap.cpp
+++ SlimGraphicsApplication/UploadHeap.cpp
@@ -2,14 +2,78 @@
 
 using namespace sg;
 
+template <typename T>
+static inline constexpr T AlignUp(T val, T align)
+{
+	return (val + align - 1) / align * align;
+}
+
 UploadHeap::UploadHeap(sg::Device* device, sg::u32 buffer_count, sg::u32 size_per_frame)
 {
-	command_buffer = device->create_command_buffer();
-
-	buffer_list.resize(buffer_count);
+	per_frame_list.resize(buffer_count);
 	for (u32 i = 0; i < buffer_count; i++)
 	{
+		PerFrameData& data = per_frame_list[i];
 		SharedPtr<Memory> upload_heap = device->allocate_memory(MemoryType::Upload, MemorySubType::None, size_per_frame, 64ull * 1024);
-		buffer_list[i] = device->create_buffer(upload_heap, size_per_frame, 64ull * 1024, BufferType::Upload, false);
+		data.buffer = device->create_buffer(upload_heap, size_per_frame, 64ull * 1024, BufferType::Upload, false);
+		data.command_list = device->create_command_buffer();
+		data.fence = device->create_queue_fence();
+		data.upload_heap_size = size_per_frame;
 	}
+}
+
+void UploadHeap::begin_frame(CommandQueue* queue)
+{
+	counter++;
+	PerFrameData& data = frame_data();
+	
+	data.upload_heap_offset = 0;
+	
+	if (data.fence_signalled)
+	{
+		queue->fence_wait_cpu(data.fence.Get(), data.fence_wait_value);
+	}
+	data.command_list->start_recording();
+}
+
+UploadHeap::Offset UploadHeap::allocate_upload_memory(sg::u32 size, sg::u32 alignment)
+{
+	UploadHeap::Offset return_offset = UploadHeap::INVALID_OFFSET;
+
+	PerFrameData& data = frame_data();
+	Offset new_offset = AlignUp(data.upload_heap_offset, alignment);
+	new_offset += size;
+
+	if (new_offset <= data.upload_heap_size)
+	{
+		return_offset = data.upload_heap_offset;
+		data.upload_heap_offset = new_offset;
+	}
+
+	return return_offset;
+}
+
+void UploadHeap::write_upload_memory(UploadHeap::Offset offset, const void* memory, sg::u32 size)
+{
+	seAssert(memory != nullptr, "expected valid ptr");
+	seAssert(size > 0, "expected valid size");
+	seAssert(offset != UploadHeap::INVALID_OFFSET, "invalid offset");
+
+	PerFrameData& data = frame_data();
+	data.buffer->write_memory(offset, memory, size);
+}
+
+void UploadHeap::upload_to_buffer(sg::Buffer* dest_buffer, sg::u32 dest_byte_offset, Offset upload_heap_offset, sg::u32 size)
+{
+	PerFrameData& data = frame_data();
+}
+
+void UploadHeap::end_frame(CommandQueue* queue)
+{
+	PerFrameData& data = frame_data();
+
+	data.command_list->end_recording();
+	queue->submit_command_list(data.command_list.get());
+	queue->fence_signal(data.fence.Get(), counter);
+	data.fence_signalled = true;
 }
--- SlimGraphicsApplication/UploadHeap.h
+++ SlimGraphicsApplication/UploadHeap.h
@@ -5,15 +5,31 @@
 class UploadHeap
 {
 public:
+	using Offset = sg::u32;
+	static constexpr Offset INVALID_OFFSET = (Offset)~0;
 	UploadHeap(sg::Device* device, sg::u32 buffer_count, sg::u32 size_per_frame);
 
-	void begin_frame();
-	void end_frame_and_submit();
+	void begin_frame(sg::CommandQueue* queue);
+	Offset allocate_upload_memory(sg::u32 size, sg::u32 alignment);
+	void write_upload_memory(Offset offset, const void* memory, sg::u32 size);
+	void upload_to_buffer(sg::Buffer* dest_buffer, sg::u32 dest_byte_offset, Offset upload_heap_offset, sg::u32 size);
+	void end_frame(sg::CommandQueue* queue);
 
 private:
-	sg::u32 active_index = 0;
-	sg::Ptr<sg::CommandList> command_buffer;
-	std::vector<sg::SharedPtr<sg::Buffer>> buffer_list;
-	std::vector<ComPtr<sg::QueueFence>> fence_list;
+	sg::u32 counter = 0;
+
+	struct PerFrameData
+	{
+		sg::Ptr<sg::CommandList> command_list;
+		sg::SharedPtr<sg::Buffer> buffer;
+		ComPtr<sg::QueueFence> fence;
+		sg::u32 upload_heap_size = 0;
+		sg::u32 fence_wait_value = 0;
+		bool fence_signalled = false;
+
+		sg::u32 upload_heap_offset = 0;
+	};
+	std::vector<PerFrameData> per_frame_list;
+	PerFrameData& frame_data() { return per_frame_list[counter % (sg::u32)per_frame_list.size()]; }
 };
 
