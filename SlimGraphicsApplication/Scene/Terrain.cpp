4b86486277e955a7d4b3c8d73c7a52e1a2b62285
AlexHoughton <houghton1411@aol.com>
2025-07-10 19:37:33 +0100

    ter

--- SlimGraphicsApplication/Scene/Terrain.cpp
+++ SlimGraphicsApplication/Scene/Terrain.cpp
@@ -1,2 +1,11 @@
 #include "Terrain.h"
 #include <lodepng.h>
+
+// https://learnopengl.com/Guest-Articles/2021/Tessellation/Height-map
+
+using namespace sg;
+
+Terrain::Terrain(SharedPtr<Device>& _device) : device(_device)
+{
+	height_textures_list.push_back("");
+}
--- SlimGraphicsApplication/Scene/Terrain.h
+++ SlimGraphicsApplication/Scene/Terrain.h
@@ -1,9 +1,18 @@
 #pragma once
+#include <sgPlatformInclude.h>
+#include "UploadHeap.h"
+#include "Model.h"
+#include "ShaderSharedStructures.h"
+#include "LinearConstantBuffer.h"
+#include "Camera.h"
+#include "DebugDraw.h"
 
 class Terrain
 {
 public:
+	Terrain(sg::SharedPtr<sg::Device>& _device);
 
 private:
-
+	std::vector<std::string> height_textures_list;
+	sg::SharedPtr<sg::Device> device;
 };
